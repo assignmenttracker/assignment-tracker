a5752909db3cad56ea19ff738472d2027cb6ae0c
Ryang Sohn <loop.infinitely@gmail.com>
2020-11-18 20:06:57 +0900

    Add IOException throwable in AssignmentProvider.fetchAssignments

--- tracker/src/main/java/com/hanahs/tracker/AssignmentProvider.java
+++ tracker/src/main/java/com/hanahs/tracker/AssignmentProvider.java
@@ -1,9 +1,10 @@
 package com.hanahs.tracker;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
 @SuppressWarnings("serial")
 public abstract class AssignmentProvider implements Serializable {
-	public abstract List<Assignment> fetchAssignments();
+	public abstract List<Assignment> fetchAssignments() throws IOException;
 }
