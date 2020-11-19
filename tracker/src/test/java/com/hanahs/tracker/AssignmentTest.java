a53c390239d6150262cc101daa0c10bd50d3063c
Ryang Sohn <loop.infinitely@gmail.com>
2020-11-19 15:26:48 +0900

    Add test for wordPool

--- tracker/src/test/java/com/hanahs/tracker/AssignmentTest.java
+++ tracker/src/test/java/com/hanahs/tracker/AssignmentTest.java
@@ -3,21 +3,23 @@ package com.hanahs.tracker;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
 class AssignmentTest {
-
 	@Test
 	void test() {
-		String name = "Physics Report";
-		String description = "Write an experiment report";
+		String name = "물리학 실험보고서";
+		String description = "실험 보고서를 작성하세요. 11월 12일까지 제출.";
 		LocalDateTime due = LocalDateTime.now();
 		Assignment assignment = new Assignment(name, description, due);
 		assertNotNull(assignment);
 		assertEquals(name, assignment.getName());
 		assertEquals(description, assignment.getDescription());
 		assertEquals(due, assignment.getDue());
+		Set<String> wordPool = assignment.getWordPool();
+		assertTrue(wordPool.contains("보고서"));
+		assertTrue(wordPool.contains("실험"));
 	}
-
 }
