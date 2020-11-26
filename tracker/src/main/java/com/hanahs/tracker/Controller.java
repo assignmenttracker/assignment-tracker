07296b90bc202747efe72396c73584c0b6ab7353
Ryang Sohn <loop.infinitely@gmail.com>
2020-11-27 08:14:52 +0900

    Add calendar display

--- tracker/src/main/java/com/hanahs/tracker/Controller.java
+++ tracker/src/main/java/com/hanahs/tracker/Controller.java
@@ -1,15 +1,74 @@
 package com.hanahs.tracker;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 
 public class Controller {
-	@FXML GridPane calendarGrid;
-	@FXML ListView accountList;
-	@FXML Label scheduleDescriptionLabel;
-	@FXML ListView scheduleList;
+	@FXML private GridPane calendarGrid;
+	@FXML private ListView accountList;
+	@FXML private Label scheduleDescriptionLabel;
+	@FXML private ListView scheduleList;
+	private int currentSelectionY = -1;
+	private int currentSelectionX = -1;
+	
+	private Node getNodeFromGridPane(int col, int row) {
+		for (Node node: calendarGrid.getChildren()) {
+			int childCol = Optional.ofNullable(GridPane.getColumnIndex(node)).orElse(-1);
+			int childRow = Optional.ofNullable(GridPane.getRowIndex(node)).orElse(-1);
+			if (childCol == col && childRow == row) return node;
+		}
+		System.out.println("Not found");
+		return null;
+	}
+	
+	@FXML public void initialize() {
+		LocalDate today = LocalDate.now();
+		LocalDate current = LocalDate.now();
+		for (int y = 0; y < 6; ++y) {
+			for (int x = 0; x < 7; ++x) {
+				if (current.equals(today) && current.getDayOfWeek().getValue() != x) continue;
+				Label label = new Label(String.valueOf(current.getDayOfMonth()));
+				label.maxHeight(Double.MAX_VALUE);
+				label.setPrefHeight(Double.MAX_VALUE);
+				label.maxWidth(Double.MAX_VALUE);
+				label.setPrefWidth(Double.MAX_VALUE);
+				label.setId(String.format("label_%d_%d", x, y + 1));
+				EventHandler<MouseEvent> handler = new EventHandler<MouseEvent>() {
+					@Override
+					public void handle(MouseEvent event) {
+						if (currentSelectionX != -1) {
+							Node lastSelectedNode = getNodeFromGridPane(currentSelectionX, currentSelectionY);
+							if (lastSelectedNode instanceof Label) {
+								Label lastSelected = (Label)lastSelectedNode;
+								lastSelected.setBorder(new Border(new BorderStroke(Color.TRANSPARENT, BorderStrokeStyle.NONE, CornerRadii.EMPTY, BorderWidths.EMPTY)));
+							}
+						}
+						Label sender = (Label)event.getSource();
+						sender.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+						currentSelectionX = GridPane.getColumnIndex(sender);
+						currentSelectionY = GridPane.getRowIndex(sender);
+					}
+				};
+				label.addEventHandler(MouseEvent.MOUSE_CLICKED, handler);
+				calendarGrid.add(label, x, y + 1);
+				current = current.plusDays(1);
+			}
+		}
+	}
 
 	@FXML public void addAccountButtonAction() {
 		System.out.println("Account Add");
