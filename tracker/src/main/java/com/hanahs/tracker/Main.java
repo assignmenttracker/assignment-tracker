a613b066ff39d5c5e5167dcba1071231233885ea
Ryang Sohn <loop.infinitely@gmail.com>
2020-11-27 06:16:41 +0900

    Change window size

--- tracker/src/main/java/com/hanahs/tracker/Main.java
+++ tracker/src/main/java/com/hanahs/tracker/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("design.fxml"));
 		primaryStage.setTitle("Test");
-		primaryStage.setScene(new Scene(root, 500, 500));
+		primaryStage.setScene(new Scene(root, 600, 400));
 		primaryStage.show();
 	}
 }
